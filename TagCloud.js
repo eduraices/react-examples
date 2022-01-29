a4d8eede1b67490b2a4abfb0c63a77ca87c24856
Eduardo <57029303+eduraices@users.noreply.github.com>
2022-01-29 18:11:52 +0100

    Add files via upload
    First job ready: split a string in a words array, need to clean all unwanted characters, yet

--- TagCloud.js
+++ TagCloud.js
@@ -1,57 +1,67 @@
-import React from 'react';
+/**
+ * Author: eduraices, 2022
+ * 
+ * 
+ * Practice to learn about handling state, filters, string edit, stats, graphs,...
+ * Level: student
+ * Requisites: React v17  - Theme-ui - javascript
+ * 
+ */
+
+// INPUT: must be passed through 'props' object from parent, fields: 'title', 'subtitle', 'body'(String array)
+// First I started with Textarea input,
+
+// OUTPUT: View from object array: String + ranking
+
+import React, { useState, useEffect} from 'react';
 /** @jsxImportSource theme-ui */
 
 import {
     Box,
     Badge,
+    Container,
+    Flex,
+    Heading,
+    Input,
+    Label,
+    Textarea
   } from 'theme-ui';
 
 /**
- * Practice to learn about handling state, filters, string edit, stats, graphs,...
- * Level: student
- * Requisites: React - Theme-ui - javascript
- * 
- */
-
-// INPUTS: Mock 'props', must be passed through 'props' object from parent, has 4 fields type String array, named: 'title', 'subtitle', 'body', 'filter' 
-// and last 1 field object with key-map, named 'dictionary'
-
-// OUTPUT: View from object array: fields: String + ranking
-
-// METHODS: 
-// 1. Slice String
-// 2. Count words
-// 3. Count pairs of words between points.
-// 4. Filter words by dictionaries
-// 5. Filter by relevance
-
-
-// funciones y hooks para el componente,
-// eventos, subscripciones y desmontaje
-//  
-
-
-
-/**
  * Nube de Tags
- * @param {*} props: campos : 'titles'[], 'subtitles'[], 'bodies'[], types: array String 
- * @returns vista arreglada de nube de tags: lógica + posición + css
+ * @param {*} 
+ * @returns view 'cloud of tags'
+ * 
+ * Jobs:
+ * 1. Split a String in words by separator and regular expression   ( Working )
+ * 2. Filter words by dictionaries                                   ( Not implemented )
+ * 3. Add synonyms, topics and categories from dictionary            ( Not yet)
+ * 4. Weigh keywords selection to build a View                      (Not Yet)
+ * 5. Define styles and sizes for the weighted Badges               (Not Yet)
  */
 
-export default function TagCloud(props) {
+export default function TagCloud() {
+
+    // not implemented yet
+    const [ filtered, setFiltered ] = useState( [] );
+    const [ words, setWords ] = useState([]);
+
     return (
+        
 
-        // TAREAS: 
-        // 1. Descomponer cadena en palabras, guardar claves de títulos y subtítulos
-        // 2. Filtrar adverbios, preposiciones, pronombres, etc.
-        // 3. Contear apariciones, patrones entre punto y punto, titulos, etc.
-        // 4. Aplicar diccionario de sinónimos y categorías generales, contear coincidencias
-        // 5. Aplicar baremo para la selección final de tags y su vista según importancia.
-
-
-
-        <Box sx={{ width: "100%", textAlign: 'center',  mx:"auto"}}>
-            <p>Tag Cloud</p>
+        <Box sx={{ width: "60%", textAlign: 'center',  mx:"auto"}}>
+            <Heading as="h4">Tag Cloud</Heading>
+            <Container>
+                <Label>Type or paste below a long paragraph</Label>
+                <Textarea onKeyUp={ (ev) => setWords(ev.target.value.split(/[\s.,;]+/))}  rows={6} />
+                <Label>{words.length - 1} words.</Label>
+            
+            </Container>
+            <Flex>
+                { words.map( word => (
+                    <Badge ml={2}>{word}</Badge>
+                ) )}
+            </Flex>
         </Box>
     )
 }
